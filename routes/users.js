24457087d2a51af841130745b628e91838f67c06
Denis Solovyev <solovyev.denis@gmail.com>
2024-07-08 20:24:53 +0300

    add errors

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { celebrate } = require('celebrate');
+const { getInfoUser } = require('../controllers/users');
+const {
+  getUserByIdValidation, updateProfileValidation,
+} = require('../utils/validation');
+
+const usersRouter = express.Router();
+const {
+  getUsers, getUser, updateUser,
+} = require('../controllers/users');
+
+usersRouter.get('/', getUsers);
+usersRouter.get('/me', getInfoUser);
+usersRouter.get('/:userId', celebrate(getUserByIdValidation), getUser);
+usersRouter.patch('/me', celebrate(updateProfileValidation), updateUser);
+
+module.exports = usersRouter;
