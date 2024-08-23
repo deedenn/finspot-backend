295ba027481defadf3cb968092271d936bf461d3
Denis Solovyev <denis@MacBook-Air-Denis.local>
2024-08-23 18:45:20 +0300

    add router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getUsers } = require('../controllers/users');
+const { getUsers, getInfoUser } = require('../controllers/users');
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', getUsers);
+usersRouter.get('/me', getInfoUser);
 
 
 module.exports = usersRouter;
